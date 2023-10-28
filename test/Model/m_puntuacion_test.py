51922a3ade9153957e214af03d4c9dddbc2a75bb
1494785 <1494785@uab.cat>
2023-10-28 18:56:56 +0200

    completar test puntacion

--- test/Model/m_puntuacion_test.py
+++ test/Model/m_puntuacion_test.py
@@ -1,10 +1,35 @@
 from src.Model.m_puntuacion import Punts
-
+import unittest
 """
     def puntuacion_maxima(self):
         return True if self.puntos >= self.maximo else False
 
 """
+def test_inicializacion():
+    puntos = Punts(100)
+
+    if puntos.get_puntos() == 0:
+        if puntos.maximo == 100:
+            print("PASSED TEST")
+        else:
+            print("[ERROR]: No se inicializa con la puntuación maxima correcta.")
+    else:
+        print("[ERROR]: Al crear la classe no empiezas con 0 puntos.")
+
+def test_sumar_puntos():
+
+    puntos = Punts(100)
+    punts_test1 = puntos.sumar_puntos(30)
+    if punts_test1 == 30:
+        punts_test2 = puntos.sumar_puntos(40)
+        if punts_test2 == 70:
+            print("PASSED TEST")
+        else:
+            print("[ERROR]: No suma correctamente quando hay puntos.")
+    else:
+        print("[ERROR]: No suma correctamente")
+
+
 
 def test_puntuacion_maxima():
     m_puntos = Punts(50)
@@ -13,3 +38,15 @@ def test_puntuacion_maxima():
         print("Se ha pasado del maximo, paramos el juego")
     else:
         print("No nos hemos pasado ni llegado al maximo")
+
+def test_get_puntos():
+    puntos = Punts(50)
+
+    if puntos.get_puntos() == 0:
+        puntos.sumar_puntos(50)
+        if puntos.get_puntos() == 50:
+            print("PASSED TEST")
+        else:
+            print("[ERROR]: No devuelve valor correcto despues de sumar.")
+    else:
+        print("[ERROR]: No devuelve valor correctp despues de inicializar.")
